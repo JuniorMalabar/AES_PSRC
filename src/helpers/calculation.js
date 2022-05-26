da61d1d052f29bb74527eb130626cb5ffc96c065
Zhukov Dmitriy <jukov.dm1tr@yandex.ru>
2022-05-27 00:47:07 +0300

    Функция получения остатка от деления полиномов

--- src/helpers/calculation.js
+++ src/helpers/calculation.js
@@ -28,6 +28,51 @@ class Calculation {
     remainderAfterDividingByAPolynomial(binary, polynom) {
         return binary % polynom
     }
+
+    remainderAfterDividingAPolynomialByAPolynomial(polynom_1, polynom_2) {
+        if (polynom_1 == polynom_2) {
+            return 0;
+        }
+
+        let positions_1 = [];
+        let positions_2 = [];
+
+        polynom_1.split("+").forEach((el) => {
+            if (el == "x") {
+            positions_1.push("1");
+            } else if (el == "1") {
+            positions_1.push("0");
+            } else {
+            positions_1.push(el[1]);
+            }
+        });
+        polynom_2.split("+").forEach((el) => {
+            if (el == "x") {
+            positions_2.push("1");
+            } else if (el == "1") {
+            positions_2.push("0");
+            } else {
+            positions_2.push(el[1]);
+            }
+        });
+        positions_1 = _.map(positions_1, (el) => {return Number(el)})
+        positions_2 = _.map(positions_2, (el) => {return Number(el)})
+        let seniorDegree_1 = positions_1[0];
+        let seniorDegree_2 = positions_2[0];
+        if (seniorDegree_1 < seniorDegree_2) {
+            return polynom_1;
+        } else {
+            while (seniorDegree_1 >= seniorDegree_2) {
+            let difference = seniorDegree_1 - seniorDegree_2;
+            let intermediateTerm = _.map(positions_2, (el) => { return el+difference})
+            let d1 = _.difference(positions_1, intermediateTerm);
+            let d2 = _.difference(intermediateTerm, positions_1);
+            positions_1 = Array.from(new Set(_.concat(d1,d2))).sort().reverse()
+            seniorDegree_1 = positions_1[0]
+            }
+        }
+        return positions_1;
+    }
 }
 
 export default Calculation;
